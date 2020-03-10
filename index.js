f4291957ce95fe745ae44d3a17d4304e856f64ad
Angel Concepcion <angelconcepcion50@gmail.com>
2020-03-10 21:28:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let musicianList = [];
+  
+  for (let i = 0; i < musicians.length; i++){
+    let currentMusician = musicians[i];
+    let currentInstrument = instrument[i];
+    
+    let string = `${currentMusician} plays ${currentInstrument}`;
+    musicianList.push(string);
+  }
+  return musicianList
+}
+
